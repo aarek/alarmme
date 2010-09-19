d4bc8b0ddef6e79926bf0dc2ee688866634795d2
matee <matee@matee.net>
2010-09-19 03:00:54 +0200

    SharePortfolio.add -> form

---
+++ alarmMe/src/main/scala/pl/dwazero/alarmme/snippet/SharePortfolios.scala
@@ -0,0 +1,47 @@
+package pl.dwazero.alarmme.snippet
+
+import _root_.scala.xml.{NodeSeq, Text}
+import _root_.net.liftweb.util.{Helpers}
+import _root_.net.liftweb.http.{RedirectResponse, RequestVar, S, SHtml}
+import pl.dwazero.alarmme.model.{SharePortfolio, User}
+import Helpers._
+
+object sp_name extends RequestVar[String]("") // maxlen -> 128
+object description extends RequestVar[String]("") // maxlen -> 8192
+object is_public extends RequestVar[Boolean](false)
+
+class SharePortfolios {
+  
+  // TODO: return Map  valid if map is empty; (name -> "Pole jest wymagane")
+  private def valid_?(sp_name: String, description: String, is_public: Boolean): Boolean = {
+    if (sp_name.toString.length < 1)
+      return false
+    // description is optional
+    return true
+  }
+  
+  def add(in: NodeSeq): NodeSeq = {
+    
+    def processCompanyAdd() = 
+      if (valid_?(sp_name, description, is_public)) {
+        val currentUser: User = User.currentUser.open_!
+        val sharePortfolio: SharePortfolio = SharePortfolio.create
+        sharePortfolio.name(sp_name)
+        sharePortfolio.description(code)
+        sharePortfolio.user(currentUser)
+        sharePortfolio.is_public(is_public)
+        val saved: Boolean = sharePortfolio.save
+        S.notice("Portfel dodany")
+        S.redirectTo("/share-portfolio/index")
+      } else {
+        S.error("Popraw błędy")
+      }
+    
+    Helpers.bind("entry", in, 
+      "name" -> SHtml.text(sp_name.is, sp_name(_), "maxlenght" -> "128"),
+      "description" -> SHtml.textarea(description.is, description(_), "maxlenght" -> "8192"),
+      "is_public" -> SHtml.checkbox(is_public.is, is_public(_)),
+      "submit" -> SHtml.submit("Dodaj", processCompanyAdd))
+  }
+}
+
